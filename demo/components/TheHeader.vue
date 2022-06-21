bfcb55ec7d2b7e5ad7c30b37ba7e5ae4afb7137a
Vladislav Kabakov <zloy.gololed@gmail.com>
2022-06-22 00:33:03 +0400

    feature: header

---
+++ demo/components/TheHeader.vue
@@ -0,0 +1,62 @@
+<template>
+  <header class="the-header">
+    <div class="the-header__inner position-fixed top-0 end-0 start-0 bg-dark">
+      <div class="container-xxl d-flex align-items-center h-100 py-2">
+        <router-link
+          to="/"
+          class="text-light me-auto align-self-stretch"
+        >
+          <IconLogo class="icon h-100 w-auto"/>
+        </router-link>
+        <nav>
+          <router-link
+            v-for="link in links"
+            :key="link.to"
+            :to="link.to"
+            class="link-light ms-3"
+          >{{ link.name }}</router-link>
+        </nav>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import IconLogo from './IconLogo.vue';
+
+export default Vue.extend({
+  name: 'TheHeader',
+  components: { IconLogo },
+  data() {
+    return {
+      links: [
+        {
+          name: 'docs',
+          to: '/docs',
+        },
+        {
+          name: 'api',
+          to: '/api',
+        },
+        {
+          name: 'demo',
+          to: '/demo',
+        },
+        {
+          name: 'cookbook',
+          to: '/cookbook',
+        },
+      ],
+    };
+  },
+});
+</script>
+
+<style lang="scss">
+.the-header {
+  &, &__inner {
+    height: 3rem;
+  }
+}
+</style>
--- demo/main.ts
+++ demo/main.ts
@@ -4,6 +4,7 @@ import App from './App.vue';
 import router from './router';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../src/settings.css';
+import './assets/index.scss';
 
 Vue.component('CarouselDemo', CarouselDemo);
 Vue.component('CarouselItem', CarouselItem);
