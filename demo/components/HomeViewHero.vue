777b218ff52a87819497286893a9b34c06129d4e
Vladislav Kabakov <zloy.gololed@gmail.com>
2022-06-22 00:34:20 +0400

    feature: home hero section

---
+++ demo/components/HomeViewHero.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hero">
+    <div class="container-xxl py-5">
+      <div class="row row-cols-2 align-items-center">
+        <div class="col">
+          <h1 class="display-1">Vue-carousel</h1>
+          <p>Modern carousel for vue with best ssr support</p>
+        </div>
+        <div class="col">
+          <div class="ratio ratio-4x3">
+            <div>
+              <div class="h-100">
+                <IconLogoAnimate
+                  class="hero__logo w-100 h-auto"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import IconLogoAnimate from './IconLogoAnimate.vue';
+
+export default {
+  name: 'HomeViewHero',
+  components: { IconLogoAnimate },
+};
+</script>
+
+<style lang="scss">
+.hero {
+  height: 100vh;
+}
+</style>
